a14b67e6f106108fbebce5230f808ba040d10af8
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-10 01:46:50 +0530

    Create Vandor schema with Mongoose and Export Vandor model for use in other modules

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Importing Dependencies
 import express from "express"
 import chalk from "chalk"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import { AdminRoute, VandorRoute } from "./routes/index.js";  
 
 // Creating an Express Application
 const app = express();
+
+// Load environment variables from a .env file into process.env
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Middleware and Routing
 // app.use('/', (req, res) => {
@@ -13,6 +22,21 @@ const app = express();
 
 app.use('/admin', AdminRoute);
 app.use('/vandor', VandorRoute);
+
+// MongoDb Connection
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => {
+    console.log(chalk.yellow("MongoDB Connected"));
+    // console.log(result)
+  })
+  .catch((err) => {
+    console.error(chalk.red("Failed to connect to MongoDB"), err);
+  });
 
 // Starting the Server
 app.listen(8000, () => {
