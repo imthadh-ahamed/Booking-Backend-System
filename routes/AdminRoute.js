aa6d4f36e8f8b10f8cf9b93dcc2bc02cb4778735
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-08-10 01:52:12 +0530

    Set up Admin routes for Vandor management

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { CreateVandor, GetVandor, GetVandorById } from "../controllers/index.js";
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.json({ message: "Hello from Admin" });
 });
+
+router.post('/createVandor', CreateVandor);
+router.get('/getVandors', GetVandor);
+router.get('/getVandor/:id', GetVandorById);
 
 export { router as AdminRoute };
